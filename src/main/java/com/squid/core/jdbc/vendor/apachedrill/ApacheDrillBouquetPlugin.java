12f0000ff251515f3c77110601839809c42328d4
hoahd <hoa@squidsolutions.com>
2016-11-24 18:03:59 +0100

    T2085

---
+++ src/main/java/com/squid/core/jdbc/vendor/apachedrill/ApacheDrillBouquetPlugin.java
@@ -0,0 +1,46 @@
+package com.squid.core.jdbc.vendor.apachedrill;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.sql.Driver;
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.squid.core.database.plugins.BaseBouquetPlugin;
+
+public class ApacheDrillBouquetPlugin extends BaseBouquetPlugin {
+
+	private static final Logger logger = LoggerFactory.getLogger(ApacheDrillBouquetPlugin.class);
+	
+	public static final String driverName =  "org.apache.drill.jdbc.Driver";
+
+	@Override
+	public void loadDriver() {
+		URL[] paths = new URL[1];
+		paths[0] = this.getClass().getProtectionDomain().getCodeSource().getLocation();
+
+		// load the driver within an isolated classLoader
+		URLClassLoader cl = new URLClassLoader(paths);
+		ClassLoader rollback = Thread.currentThread().getContextClassLoader();
+		Thread.currentThread().setContextClassLoader(cl);
+
+		this.drivers = new ArrayList<Driver>();
+		
+			Driver driver;
+			try {
+				driver = (Driver) Class.forName(driverName, true, cl).newInstance();
+				drivers.add(driver) ;
+
+			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+				logger.info("Failed to instanciate driver " +driverName);
+				e.printStackTrace();
+			}
+
+		Thread.currentThread().setContextClassLoader(rollback);
+
+	}
+
+
+}
