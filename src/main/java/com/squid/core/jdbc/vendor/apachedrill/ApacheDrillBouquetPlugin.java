60550bf99afb7e064126313e7372f28a84e18ff2
hoahd <hoa@squidsolutions.com>
2016-11-30 15:26:35 +0100

    T2085

--- src/main/java/com/squid/core/jdbc/vendor/apachedrill/ApacheDrillBouquetPlugin.java
+++ src/main/java/com/squid/core/jdbc/vendor/apachedrill/ApacheDrillBouquetPlugin.java
@@ -13,8 +13,8 @@ import com.squid.core.database.plugins.BaseBouquetPlugin;
 public class ApacheDrillBouquetPlugin extends BaseBouquetPlugin {
 
 	private static final Logger logger = LoggerFactory.getLogger(ApacheDrillBouquetPlugin.class);
-	
-	public static final String driverName =  "org.apache.drill.jdbc.Driver";
+
+	public static final String driverName = "org.apache.drill.jdbc.Driver";
 
 	@Override
 	public void loadDriver() {
@@ -22,25 +22,24 @@ public class ApacheDrillBouquetPlugin extends BaseBouquetPlugin {
 		paths[0] = this.getClass().getProtectionDomain().getCodeSource().getLocation();
 
 		// load the driver within an isolated classLoader
-		URLClassLoader cl = new URLClassLoader(paths);
+		this.driverCL = new URLClassLoader(paths);
 		ClassLoader rollback = Thread.currentThread().getContextClassLoader();
-		Thread.currentThread().setContextClassLoader(cl);
+		Thread.currentThread().setContextClassLoader(driverCL);
 
 		this.drivers = new ArrayList<Driver>();
-		
-			Driver driver;
-			try {
-				driver = (Driver) Class.forName(driverName, true, cl).newInstance();
-				drivers.add(driver) ;
 
-			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-				logger.info("Failed to instanciate driver " +driverName);
-				e.printStackTrace();
-			}
+		Driver driver;
+		try {
+			driver = (Driver) Class.forName(driverName, true, driverCL).newInstance();
+			drivers.add(driver);
+
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			logger.info("Failed to instanciate driver " + driverName + " for plug in apache drill");
+			e.printStackTrace();
+		}
 
 		Thread.currentThread().setContextClassLoader(rollback);
 
 	}
-
 
 }
