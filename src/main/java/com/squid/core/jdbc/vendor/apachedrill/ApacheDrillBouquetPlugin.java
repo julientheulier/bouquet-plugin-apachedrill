a5abd7985c750aa5d45ba579302c1fca2e420e9f
hoahd <hoa@squidsolutions.com>
2016-12-14 16:33:15 +0100

    T1828

--- src/main/java/com/squid/core/jdbc/vendor/apachedrill/ApacheDrillBouquetPlugin.java
+++ src/main/java/com/squid/core/jdbc/vendor/apachedrill/ApacheDrillBouquetPlugin.java
@@ -1,3 +1,27 @@
+/*******************************************************************************
+ * Copyright © Squid Solutions, 2016
+ *
+ * This file is part of Open Bouquet software.
+ *  
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation (version 3 of the License).
+ *
+ * There is a special FOSS exception to the terms and conditions of the 
+ * licenses as they are applied to this program. See LICENSE.txt in
+ * the directory of this program distribution.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Squid Solutions also offers commercial licenses with additional warranties,
+ * professional functionalities or services. If you purchase a commercial
+ * license, then it supersedes and replaces any other agreement between
+ * you and Squid Solutions (above licenses and LICENSE.txt included).
+ * See http://www.squidsolutions.com/EnterpriseBouquet/
+ *******************************************************************************/
+
 package com.squid.core.jdbc.vendor.apachedrill;
 
 import java.net.URL;
@@ -8,6 +32,7 @@ import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.squid.core.database.impl.DriverShim;
 import com.squid.core.database.plugins.BaseBouquetPlugin;
 
 public class ApacheDrillBouquetPlugin extends BaseBouquetPlugin {
@@ -40,6 +65,14 @@ public class ApacheDrillBouquetPlugin extends BaseBouquetPlugin {
 
 		Thread.currentThread().setContextClassLoader(rollback);
 
+	}
+	@Override
+	public ArrayList<DriverShim> getDrivers() {
+		ArrayList<DriverShim> shims  = new ArrayList<DriverShim>();
+		for (Driver d : this.drivers){
+			shims.add(new ApacheDrillDriverShim(d));			
+		}		
+		return shims;		
 	}
 
 }
